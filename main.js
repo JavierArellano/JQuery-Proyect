4073f73e97c920a741afa46f1073f781ab4c4014
Javier Arellano <franjarca@hotmail.com>
2017-01-19 09:47:18 +0100

    proyecto jquery

---
+++ main.js
@@ -0,0 +1,34 @@
+var cont = 0;
+$(document).ready(function(){
+    $("#buscar").click(function(){
+        nueva_busqueda();
+        let texto_buscar = $("input").val();
+        $.getJSON("http://www.omdbapi.com/?s="+texto_buscar, function(pelis){
+            for (peli of pelis.Search){
+                inserta_pelis(peli);
+            }
+        });
+    });
+    $(window).scroll(function() {
+        if($(window).scrollTop() + $(window).height() >= $(document).height()-10) {
+            obtener_datos();
+        }
+    });
+    
+});
+function obtener_datos(){
+    cont++;
+    let texto_buscar = $("input").val();
+    $.getJSON("http://www.omdbapi.com/?s="+texto_buscar+"&page="+cont, function(pelis){
+        for (peli of pelis.Search){
+            inserta_pelis(peli);
+        }
+    });
+}
+function nueva_busqueda(){
+    cont=0;
+    $("#contenedor").empty();
+}
+function inserta_pelis(pelis){
+    $("#contenedor").append($("<div id='tarjeta'><img src="+pelis.Poster+"><p>"+pelis.Title+"</p></div>"));
+}
